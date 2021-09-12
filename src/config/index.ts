5b12af525274379f930338baed2e07a1685694e9
Eli-BB8 <elikovner1@gmail.com>
2021-09-12 15:58:46 +0300

    nothing

--- src/config/index.ts
+++ src/config/index.ts
@@ -127,7 +127,7 @@ const config = {
             {
               method: 'startsWithCondition',
               field: 'hierarchy',
-              value: 'es_name/',
+              value: 'es_name',
             },
           ],
         },
