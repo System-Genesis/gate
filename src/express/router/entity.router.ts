614c158b6c0ee48e652b84ca0ef7587aa5166816
NoamYes <noamiko600@gmail.com>
2021-09-23 11:32:33 +0300

    feat: add identifier route

--- src/express/router/entity.router.ts
+++ src/express/router/entity.router.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
 // import { Router } from 'express';
-import Controller from '../controller/controller';
-import wrapController from '../../utils/wrapController';
-import { setEntityType, setService } from '../middlewares';
-import config from '../../config/index';
+import Controller from "../controller/controller";
+import wrapController from "../../utils/wrapController";
+import { setEntityType, setService } from "../middlewares";
+import config from "../../config/index";
 
 const {
   web: {
@@ -13,82 +13,88 @@ const {
 
 const entityRouter: Router = Router();
 
-entityRouter.use(wrapController(setEntityType('entity')));
+entityRouter.use(wrapController(setEntityType("entity")));
 
 entityRouter.get(
-  '/search',
+  "/search",
   wrapController(setService(elastic)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/role/:roleId',
+  "/identifier/:identifier",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/digitalIdentity/:uniqueID',
+  "/role/:roleId",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/group/:groupId',
+  "/digitalIdentity/:uniqueID",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/hierarchy/:hierarchy',
+  "/group/:groupId",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/:id',
+  "/hierarchy/:hierarchy",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.get(
-  '/',
+  "/:id",
+  wrapController(setService(read)),
+  wrapController(Controller.proxyRequest)
+);
+
+entityRouter.get(
+  "/",
   wrapController(setService(read)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.patch(
-  '/:id/connectDigitalIdentity',
+  "/:id/connectDigitalIdentity",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.patch(
-  '/:id/disconnectDigitalIdentity',
+  "/:id/disconnectDigitalIdentity",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.patch(
-  '/:id/deactivate',
+  "/:id/deactivate",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.patch(
-  '/:id',
+  "/:id",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.delete(
-  '/:id',
+  "/:id",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
 
 entityRouter.post(
-  '/',
+  "/",
   wrapController(setService(write)),
   wrapController(Controller.proxyRequest)
 );
