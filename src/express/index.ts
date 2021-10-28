4b49607c5847cbc3e8c22fabe59d2de5aef45a9e
NoamYes <noamiko600@gmail.com>
2021-10-28 15:30:12 +0300

    (fix): remove problematic scopes configuration arrayMapper

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,5 +1,7 @@
+import fs from 'fs';
 import express from 'express';
 // import helmet from "helmet";
+import compression from 'compression';
 import morgan from 'morgan';
 import router from './router/router';
 import isAuth from '../auth/auth';
@@ -13,7 +15,15 @@ const app = express();
 // app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('dev', {
+    stream: fs.createWriteStream('./access.log', { flags: 'a' }),
+  })
+);
 app.use(morgan('dev'));
+
+app.use(compression());
 
 // app.use((req, _, next) => {
 //   if (req.headers["authorization"] === process.env.AUTH) {
