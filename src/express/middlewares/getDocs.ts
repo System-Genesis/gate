04a4dc874f89280bf1b76e15aa848dc0abe9899a
Eli-BB8 <elikovner1@gmail.com>
2021-12-13 16:45:41 +0200

    delete swagger.yaml from root

--- src/express/middlewares/getDocs.ts
+++ src/express/middlewares/getDocs.ts
@@ -1,11 +1,10 @@
 import * as YAML from 'yamljs';
 import * as swaggerTools from 'swagger-tools';
 import { Request, Response, NextFunction } from 'express';
-import path from 'path';
 
 export const getDocsMiddleware = () => {
   // load swagger docs
-  const swaggerDoc = YAML.load(path.resolve('../../config/volume/swagger.yaml'));
+  const swaggerDoc = YAML.load('swagger.yaml');
   // get middleware promise
   const loadSwaggerPromise = new Promise((resolve, reject) => {
     swaggerTools.initializeMiddleware(swaggerDoc, (middleware: any) => resolve(middleware));
