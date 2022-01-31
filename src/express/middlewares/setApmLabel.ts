407db24d73d247c0803b08ef29206245edd35136
NoamYes <noamiko600@gmail.com>
2022-01-31 10:42:20 +0200

    (fixed) uncomment apm

--- src/express/middlewares/setApmLabel.ts
+++ src/express/middlewares/setApmLabel.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-// import { apmAgent } from "../.."
+import { apmAgent } from "../.."
 import { extractClientName } from "../../helpers"
 
 
 export const setApmLabel = (req: Request, res: Response, next: NextFunction) => {
     const clientName = extractClientName(req.headers['authorization'] || '');
     console.log(clientName);
-    
-//     apmAgent.setLabel('clientName', clientName);
+
+    apmAgent.setLabel('clientName', clientName);
     next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 // Add this to the VERY top of the first 
 // file loaded in your app
-// import apm from 'elastic-apm-node';
-// export const apmAgent = apm.start({
-//     // Override service name from package.json
-//     // Allowed characters: a-z, A-Z, 0-9, -, _, 
-//     // and space
-//     serviceName: 'gate',
+import apm from 'elastic-apm-node';
+export const apmAgent = apm.start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, 
+    // and space
+    serviceName: 'gate',
 
-//     // Use if APM Server requires a token
-//     secretToken: process.env['APM_SECRET_TOKEN'],
+    // Use if APM Server requires a token
+    secretToken: process.env['APM_SECRET_TOKEN'],
 
-//     // Set custom APM Server URL
-//     // Default: http://localhost:8200
-//     serverUrl: process.env['APM_SERVER'],
-// });
+    // Set custom APM Server URL
+    // Default: http://localhost:8200
+    serverUrl: process.env['APM_SERVER'],
+});
 
 import Server from './express';
 import config from './config';
 const { web } = config;
 
-//!apmAgent.isStarted() || console.log('apm-agent is started');
+!apmAgent.isStarted() || console.log('apm-agent is started');
 
 const main = async () => {
     await Server(web.port);
