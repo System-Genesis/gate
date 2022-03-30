e40e4093a13e8200150ac1406ecc86e7de702185
Eli-BB8 <elikovner1@gmail.com>
2022-03-30 10:09:02 +0300

    test - add testing for more coverage

--- tests/transformService.test.ts
+++ tests/transformService.test.ts
@@ -1,138 +1,187 @@
 import { applyTransform } from '../src/transformService';
+import { EntityDTO } from '../src/types';
 
 const person = {
-    firstName: 'yuu',
-    lastName: 'yoo',
-    directGroup: 'asdasdasd',
-    identityCard: '11111111',
-    entityType: 'agumon',
-    job: 'weatherMan',
-    hierarchy: ['root', 'sensitive2'],
-    domainUsers: [
-        {
-            uniqueID: 'aaa@yay',
-            adfsUID: 'aaa@yay.com',
-            hierarchy: ['lol', 'sensitive2'],
-            dataSource: 'dataSource2',
-        },
-        {
-            uniqueID: 'rrr@es',
-            adfsUID: 'rrr@es.com',
-            hierarchy: ['lol', 'sensitive2'],
-            dataSource: 'es',
-        },
-        {
-            uniqueID: 'aaa@ser',
-            adfsUID: 'aaa@ser.com',
-            hierarchy: ['lol', 'sensitive2'],
-            dataSource: 'dataSource2',
-        },
-        {
-            uniqueID: 'aa22a@ser',
-            adfsUID: 'aaa22@s22er.com',
-            hierarchy: ['asdsad', 'asdasd'],
-            dataSource: 'dataSource1',
-        },
+    id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    displayName: "root/father/child - John Doe",
+    directGroup: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    hierarchy: "root/father/child",
+    entityType: "agumon",
+    identityCard: "234567891",
+    personalNumber: "3456712",
+    serviceType: "Elder",
+    firstName: "John",
+    status: "active",
+    goalUserId: "spr_davidh@oneAman",
+    lastName: "Doe",
+    fullName: "John Doe",
+    akaUnit: "Lions",
+    dischargeDay: "2016-08-29T0:1:33.001Z",
+    rank: "matador",
+    mail: "you@your-company.com",
+    jobTitle: "clown",
+    phone: [
+        "09-0000000",
+        "012222222"
     ],
-};
+    mobilePhone: [
+        "051-0000000",
+    ],
+    address: "962 Darion Streets, Irving",
+    clearance: "6",
+    pictures: {
+        profile: {
+            path: "http://devante.net",
+            meta: {
+                createdAt: "2022-02-23T0:4:59.251Z",
+                updatedAt: "2022-02-23T0:4:59.251Z"
+            }
+        }
+    },
+    sex: "male",
+    birthDate: "2016-08-29T0:1:33.001Z",
+    createdAt: "2016-08-29T0:1:33.001Z",
+    updatedAt: "2016-08-29T0:1:33.001Z",
+    digitalIdentities: [
+        {
+            type: "tamar",
+            source: "es",
+            mail: "you@your-company.com",
+            uniqueId: "uniqueId@domain",
+            createdAt: "2022-02-23T0:4:59.251Z",
+            updatedAt: "2022-02-23T0:4:59.251Z",
+            entityId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            isRoleAttachable: true,
+            role: {
+                roleId: "name@domain",
+                jobTitle: "super clown",
+                digitalIdentityUniqueId: "you@your-company.com",
+                directGroup: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                hierarchy: "root/father/child",
+                hierarchyIds: [
+                    "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                ],
+                source: "ads",
+                clearence: "red",
+                createdAt: "2022-02-23T0:4:59.251Z",
+                updatedAt: "2022-02-23T0:4:59.251Z"
+            }
+        }
+    ]
+}
 
 const person2 = {
-    firstName: 'yuu',
-    lastName: 'yoo',
-    directGroup: 'asdasdasd',
-    identityCard: '11111111',
-    entityType: 'agumon',
-    job: 'weatherMan',
-    hierarchy: ['good', 'notsensitive'],
-    domainUsers: [
-        {
-            uniqueID: 'aaa@yay',
-            adfsUID: 'aaa@yay.com',
-            hierarchy: ['lol', 'notsensitive2'],
-            dataSource: 'dataSource23',
-        },
-        {
-            uniqueID: 'rrr@es',
-            adfsUID: 'rrr@es.com',
-            hierarchy: ['lol', 'notsensitive2'],
-            dataSource: 'es',
-        },
-        {
-            uniqueID: 'aaa@ser',
-            adfsUID: 'aaa@ser.com',
-            hierarchy: ['lol', 'notsensitive2'],
-            dataSource: 'dataSource23',
-        },
-        {
-            uniqueID: 'aa22a@ser',
-            adfsUID: 'aaa22@s22er.com',
-            hierarchy: ['asdsad', 'asdasd'],
-            dataSource: 'dataSource5',
-        },
+    id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    displayName: "root/father/child - John Doe",
+    directGroup: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    hierarchy: "root/father/child",
+    entityType: "agumon",
+    identityCard: "234567891",
+    personalNumber: "3456712",
+    serviceType: "Elder",
+    firstName: "John",
+    status: "active",
+    goalUserId: "spr_davidh@oneAman",
+    lastName: "Doe",
+    fullName: "John Doe",
+    akaUnit: "Lions",
+    dischargeDay: "2016-08-29T0:1:33.001Z",
+    rank: "matador",
+    mail: "you@your-company.com",
+    jobTitle: "clown",
+    phone: [
+        "09-0000000",
+        "012222222"
     ],
-};
+    mobilePhone: [
+        '051-0000000',
+        '592222222'
+    ],
+    address: "962 Darion Streets, Irving",
+    clearance: "6",
+    pictures: {
+        profile: {
+            path: "http://devante.net",
+            meta: {
+                createdAt: "2022-02-23T0:4:59.246Z",
+                updatedAt: "2022-02-23T0:4:59.246Z"
+            }
+        }
+    },
+    sex: "male",
+    birthDate: "2016-08-29T0:1:33.001Z",
+    createdAt: "2016-08-29T0:1:33.001Z",
+    updatedAt: "2016-08-29T0:1:33.001Z"
+}
 
 describe('test transformer removing needed stuff', () => {
-    test('remove job', async () => {
-        const scopes = ['removeJob'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson).not.toHaveProperty('job');
+    describe('fieldEclude', () => {
+        test('remove job', async () => {
+            const scopes = ['externalScope'];
+            const moddedPerson = applyTransform(person, scopes, 'entity');
+            expect(moddedPerson).not.toHaveProperty('jobTitle');
+        });
+    
+        test('remove sensitive hierarchy', async () => {
+            const scopes = ['externalScope'];
+            const moddedPerson = applyTransform(person2, scopes, 'entity');
+    
+            expect(moddedPerson).not.toHaveProperty('hierarchy');
+        });
+    
+        test('remove digitalIdentities', async () => {
+            const scopes = ['externalScope'];
+            const moddedPerson = applyTransform(person, scopes, 'entity');
+            expect(moddedPerson).not.toHaveProperty('digitalIdentities');
+        });
+    
+        test('remove digitalIdentities', async () => {
+            const scopes = ['externalScope'];
+            const moddedPerson = applyTransform(person, scopes, 'entity');
+            expect(moddedPerson).not.toHaveProperty('digitalIdentities');
+        });
     });
 
-    test('remove sensitive hierarchy', async () => {
-        const scopes = ['removeSensitive2Hierarchy'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson).not.toHaveProperty('hierarchy');
+    describe('arrayMapper', () => {
+        test('remove mail from digitalIdentities', async () => {
+            const scopes = ['myScope'];
+            const moddedPerson = applyTransform(person, scopes, 'entity');
+            // expect(moddedPerson).toHaveProperty('digitalIdentities');
+            expect((moddedPerson as EntityDTO).digitalIdentities![0]).toHaveProperty('mail');
+        });
     });
 
-    test('remove sensitive hierarchy from domainUsers', async () => {
-        const scopes = ['removeSensitive2DomainUsersHierarchy'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson.domainUsers[0]).not.toHaveProperty('hierarchy');
-        expect(moddedPerson.domainUsers[2]).not.toHaveProperty('hierarchy');
-        expect(moddedPerson.domainUsers).toHaveLength(4);
+    describe('arrayFilter', () => {
+        test('remove digitalIdentities element', async () => {
+            const scopes = ['someScope'];
+            const moddedPerson = applyTransform(person, scopes, 'entity');
+            expect((moddedPerson as EntityDTO).digitalIdentities!).toStrictEqual([]);
+        });
     });
 
-    test('remove sensitive domainUsers', async () => {
-        const scopes = ['removeSensitiveDomainUsers'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson.domainUsers).toHaveLength(3);
-    });
-
-    test('remove sensitive directGroup', async () => {
-        const scopes = ['removeSensitive2DirectGroup'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson).not.toHaveProperty('directGroup');
-    });
-
-    test('remove multiple things', async () => {
-        const scopes = ['removeSensitive2DomainUsersHierarchy', 'removeSensitive2Hierarchy', 'removeJob', 'removeSensitiveDomainUsers'];
-        const moddedPerson = applyTransform(person, scopes, 'entity');
-        expect(moddedPerson.domainUsers[0]).not.toHaveProperty('hierarchy');
-        expect(moddedPerson.domainUsers[2]).not.toHaveProperty('hierarchy');
-        expect(moddedPerson).not.toHaveProperty('hierarchy');
-        expect(moddedPerson).not.toHaveProperty('job');
-        expect(moddedPerson.domainUsers).toHaveLength(3);
-    });
 });
 
 describe('test transformer not removing if condition didnt happen', () => {
-    test('remove sensitive hierarchy', async () => {
-        const scopes = ['removeSensitive2Hierarchy'];
-        const moddedPerson = applyTransform(person2, scopes, 'entity');
-        expect(moddedPerson).toHaveProperty('hierarchy');
+    test('not remove sensitive hierarchy', async () => {
+        const scopes = ['myScope'];
+        const { hierarchy, ...personWithoutHirarchy } = person;
+        const moddedPerson = applyTransform(personWithoutHirarchy, scopes, 'entity');
+        expect(moddedPerson).toHaveProperty('firstName');
     });
+});
 
-    test('remove sensitive domainUsers', async () => {
-        const scopes = ['removeSensitiveDomainUsers'];
-        const moddedPerson = applyTransform(person2, scopes, 'entity');
-        expect(moddedPerson.domainUsers).toHaveLength(4);
+describe('test transformer removing without condition', () => {
+    test('remove digitalIdentities element', async () => {
+        const scopes = ['someOtherScope'];
+        const moddedPerson = applyTransform(person, scopes, 'entity');
+        expect(moddedPerson).not.toHaveProperty('directGroup');
     });
+});
 
-    test('remove sensitive hierarchy from domainUsers', async () => {
-        const scopes = ['removeSensitive2DomainUsersHierarchy'];
-        const moddedPerson = applyTransform(person2, scopes, 'entity');
-        expect(moddedPerson.domainUsers[0]).toHaveProperty('hierarchy');
-        expect(moddedPerson.domainUsers[2]).toHaveProperty('hierarchy');
+describe('test user without scopes', () => {
+    test('if nothing changed', async () => {
+        const scopes = [];
+        const moddedPerson = applyTransform(person, scopes, 'entity');
+        expect(moddedPerson).toStrictEqual(person);
     });
 });
