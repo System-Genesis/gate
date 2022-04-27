15c47d5aca58858610be81d949ed5c5d5c1b221a
tommyafek <tommyafek@gmail.com>
2022-04-27 13:37:11 +0300

    (group leaf test)

---
+++ integration/write/groupLeaf.test.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+//import testJson from "../../src/config/test.json";
+import { app } from "../../src/express";
+import { sleep } from "../../src/utils/indexTest";
+
+
+
+describe("Testing leaf update", () => {
+    const fatherGroupToCreate: any = {
+        name: "city_name",
+        source: "city_name",
+        diPrefix: "1234421"
+    };
+
+    afterEach(() => {
+        sleep(1000);
+    });
+
+    let fatherGroupId;
+    it("should create root group with leaf true", (done) => {
+        request(app).post("/api/groups").send(fatherGroupToCreate).expect(200)
+            .end(async (err: any, res: any) => {
+                if (err) {
+                    console.log(err);
+                    throw done(err);
+                }
+                fatherGroupId = res.body.id;
+                expect(fatherGroupId).toBeDefined();
+                expect(res.body.isLeaf).toBeTruthy()
+
+                return done();
+            });
+
+    })
+
+    let firstChildGroupId;
+    it("should create root group with leaf true", (done) => {
+        const fatherGroupToCreate: any = {
+            name: "lol",
+            source: "city_name",
+            diPrefix: "1234421",
+            directGroup: fatherGroupId
+        };
+        request(app).post("/api/groups").send(fatherGroupToCreate).expect(200)
+            .end(async (err: any, res: any) => {
+                if (err) {
+                    console.log(err);
+                    throw done(err);
+                }
+                firstChildGroupId = res.body.id;
+                expect(firstChildGroupId).toBeDefined();
+
+                return done();
+            });
+
+    })
+
+})
