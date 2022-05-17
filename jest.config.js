66889107ba583362ba14c7eb34174a5881011cef
tommyafek <tommyafek@gmail.com>
2022-05-17 10:46:30 +0300

    (tests): working correctly, please remmember to delete db collections on npm run test

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
+  setTimeout: 5000000,
+  setupFilesAfterEnv: ["./jest.setup.js"],
+
   runFirstly: ["routes.test.ts"],
 };
